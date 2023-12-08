e43603f3950881ec46c64e4cef7f9fc02cc8631e
bidursapkota00 <bidursapkota00@gmail.com>
2023-12-08 20:59:36 +0545

    Problem of moving nav items daraz logistics partner to right solved, added absolute to box

--- app/_components/navbar/box.tsx
+++ app/_components/navbar/box.tsx
@@ -1,36 +1,38 @@
 import Link from "next/link"
 import Person4Icon from '@mui/icons-material/Person4';
 
-export default function Box({ position1 = "mt-[10px]", position2 = "left-[50%]" }) {
+export default function Box({ positionArrow = "left-[50%]" }) {
     return (
-        <div className={`${position1} relative z-50 cursor-default text-black bg-white border rounded-[5px] border-[#bfbfbf] hidden  group-hover:block transition-all duration-1000`}>
-            <div className='relative'>
-                <div className={`absolute w-[10px] h-[10px] border-l border-t border-[#bfbfbf] top-0 ${position2} -translate-x-[50%] -translate-y-[50%] rotate-[45deg] bg-white`}></div>
-                <div className="flex flex-row p-[30px] flex-wrap w-[250px]">
-                    <Link href="#" className='flex flex-row cursor-pointer pb-[10px]'>
-                        <Person4Icon sx={{ fontSize: 20, color: '#6e686e' }} />
-                        <span className="text-[12px] text-[#6e686e] ml-[20px] w-[120px]">Help Center</span>
-                    </Link>
-                    <Link href="#" className='flex flex-row cursor-pointer py-[10px]'>
-                        <Person4Icon sx={{ fontSize: 20, color: '#6e686e' }} />
-                        <span className="text-[12px] text-[#6e686e] ml-[20px] w-[120px]">Chat with Us</span>
-                    </Link>
-                    <Link href="#" className='flex flex-row cursor-pointer py-[10px]'>
-                        <Person4Icon sx={{ fontSize: 20, color: '#6e686e' }} />
-                        <span className="text-[12px] text-[#6e686e] ml-[20px] w-[120px]">Order</span>
-                    </Link>
-                    <Link href="#" className='flex flex-row cursor-pointer py-[10px]'>
-                        <Person4Icon sx={{ fontSize: 20, color: '#6e686e' }} />
-                        <span className="text-[12px] text-[#6e686e] ml-[20px] w-[120px]">Shipping & Delivery</span>
-                    </Link>
-                    <Link href="#" className='flex flex-row cursor-pointer py-[10px]'>
-                        <Person4Icon sx={{ fontSize: 20, color: '#6e686e' }} />
-                        <span className="text-[12px] text-[#6e686e] ml-[20px] w-[120px]">Payment</span>
-                    </Link>
-                    <Link href="#" className='flex flex-row cursor-pointer pt-[10px]'>
-                        <Person4Icon sx={{ fontSize: 20, color: '#6e686e' }} />
-                        <span className="text-[12px] text-[#6e686e] ml-[20px] w-[120px]">Returns & Refund</span>
-                    </Link>
+        <div className="absolute z-50 top-[calc(100%+12px)] left-[50%] -translate-x-[50%]">
+            <div className='relative cursor-default text-black bg-white border rounded-[5px] border-[#bfbfbf] hidden  group-hover:block transition-all duration-1000'>
+                <div className='relative'>
+                    <div className={`absolute w-[10px] h-[10px] border-l border-t border-[#bfbfbf] top-0 ${positionArrow} -translate-x-[50%] -translate-y-[50%] rotate-[45deg] bg-white`}></div>
+                    <div className="flex flex-row p-[30px] flex-wrap w-[250px]">
+                        <Link href="#" className='flex flex-row cursor-pointer pb-[10px]'>
+                            <Person4Icon sx={{ fontSize: 20, color: '#6e686e' }} />
+                            <span className="text-[12px] text-[#6e686e] ml-[20px] w-[120px]">Help Center</span>
+                        </Link>
+                        <Link href="#" className='flex flex-row cursor-pointer py-[10px]'>
+                            <Person4Icon sx={{ fontSize: 20, color: '#6e686e' }} />
+                            <span className="text-[12px] text-[#6e686e] ml-[20px] w-[120px]">Chat with Us</span>
+                        </Link>
+                        <Link href="#" className='flex flex-row cursor-pointer py-[10px]'>
+                            <Person4Icon sx={{ fontSize: 20, color: '#6e686e' }} />
+                            <span className="text-[12px] text-[#6e686e] ml-[20px] w-[120px]">Order</span>
+                        </Link>
+                        <Link href="#" className='flex flex-row cursor-pointer py-[10px]'>
+                            <Person4Icon sx={{ fontSize: 20, color: '#6e686e' }} />
+                            <span className="text-[12px] text-[#6e686e] ml-[20px] w-[120px]">Shipping & Delivery</span>
+                        </Link>
+                        <Link href="#" className='flex flex-row cursor-pointer py-[10px]'>
+                            <Person4Icon sx={{ fontSize: 20, color: '#6e686e' }} />
+                            <span className="text-[12px] text-[#6e686e] ml-[20px] w-[120px]">Payment</span>
+                        </Link>
+                        <Link href="#" className='flex flex-row cursor-pointer pt-[10px]'>
+                            <Person4Icon sx={{ fontSize: 20, color: '#6e686e' }} />
+                            <span className="text-[12px] text-[#6e686e] ml-[20px] w-[120px]">Returns & Refund</span>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
--- app/_components/navbar/navbar.tsx
+++ app/_components/navbar/navbar.tsx
@@ -10,16 +10,14 @@ import Box from "./box";
 export default function Navbar() {
     return (
         <div className='bg-[#f85606] h-[91px] px-[76px]'>
-            {/*  */}
-            <div className="flex flex-row justify-between       h-[30px]">
-                {/*  */}
-                <div className="flex flex-row      pt-[5px] h-[20px]">
+            <div className="flex flex-row justify-between h-[30px]">
+                <div className="flex flex-row pt-[5px] h-[20px]">
                     <Link href="#" className="mr-[20px] text-[12px] text-[#fff] hover:text-[#eee] cursor-pointer">Become a Seller</Link>
                     <Link href="#" className="mr-[20px] text-[12px] text-[#fff] hover:text-[#eee] cursor-pointer">Payments & Recharge</Link>
-                    {/*  */}
-                    <div className="group mr-[20px] text-[12px] text-[#fff] hover:text-[#eee] hover:h-[30px] cursor-pointer">
+                    <div className="group relative mr-[20px] text-[12px] text-[#fff] hover:text-[#eee] cursor-pointer">
                         <Link href="#" className="">Help & Support</Link>
-                        <Box position2={'left-[50%]'} />
+                        <div className="absolute top-[100%] left-0 h-[15px] w-[100%]"></div>
+                        <Box />
                     </div>
                     <Link href="#" className="mr-[20px] text-[12px] text-[#fff] hover:text-[#eee] cursor-pointer">Daraz Logistics Partner</Link>
                 </div>
@@ -43,12 +41,12 @@ export default function Navbar() {
                         <span className="text-[12px] text-[#fff] font-bold mr-[10px]">|</span>
                         <Link href='#' className="text-[12px] text-[#fff] font-bold p-[10px] hover:bg-[rgba(0,0,0,0.1)] rounded-[10px] cursor-pointer">Sign Up</Link>
                     </div>
-                    <div className="flex flex-row items-center mr-[10px] p-[10px] hover:bg-[rgba(0,0,0,0.1)] rounded-[10px]">
+                    <div className="flex flex-row items-center mr-[10px] p-[10px] hover:bg-[rgba(0,0,0,0.1)] rounded-[10px] cursor-pointer">
                         <LanguageIcon sx={{ fontSize: 20, color: '#fff', marginRight: '5px' }} />
                         <span className="text-[12px] text-[#fff] font-bold mr-[5px]">EN</span>
                         <KeyboardArrowDownIcon sx={{ fontSize: 15, color: '#fff' }} />
                     </div>
-                    <div className="py-[5px] px-[9px] hover:bg-[rgba(0,0,0,0.1)] rounded-[10px]">
+                    <div className="py-[5px] px-[9px] hover:bg-[rgba(0,0,0,0.1)] rounded-[10px] cursor-pointer">
                         <ShoppingCartOutlinedIcon className=" " sx={{ fontSize: 20, color: '#fff' }} />
                     </div>
                 </div>
