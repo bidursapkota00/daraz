4cf88c60af3ec5580227cbd2c1e89529d76f8c3c
bidursapkota00 <bidursapkota00@gmail.com>
2023-12-13 21:56:04 +0545

    slider done

--- app/_components/hero/hero.tsx
+++ app/_components/hero/hero.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import SliderPage from './slider'
 import WomanIcon from '@mui/icons-material/Woman';
+import Link from 'next/link';
 
 interface Category {
     icon: ReactNode,
@@ -34,8 +35,8 @@ export default function Hero() {
         { key: 'Beauty Tools', category: [{ image: '', category: 'Flat Irons' }, { image: '', category: 'Hair Dryer' }] },
     ]
     return (
-        <div className='flex flex-row py-[10px] px-[76px]'>
-            <div className="w-[20%] relative border border-[#bfbfbf] mr-[50px]">
+        <div className='flex flex-row py-[10px] px-[76px] bg-[#f5f5f5]'>
+            <div className="w-[20%] relative border border-[#bfbfbf] bg-white mr-[50px]">
                 {categories.map((c, i) => <div key={i} className="grid grid-cols-[30px_1fr_0px] cursor-pointer group w-[100%]">
                     {c.icon}
                     <span>{c.text}</span>
@@ -43,24 +44,28 @@ export default function Hero() {
                         <div className="absolute z-50 left-[100%] top-0 h-[100%] w-[15px]"></div>
                     </div>
                     {subCategories.map((sc, i) => {
-                        if (sc.key == c.key) return <div key={i} className="absolute z-50 left-[calc(100%+10px)] top-0 min-h-[100%] w-[200px] hidden group-hover:block border border-[#bfbfbf]">
-                            {sc.category.map((sc1, i) => <div className='relative group/sub' key={i}>
-                                <span>{sc1}</span>
-                                {subCategories2.map((sc2, i) => {
-                                    if (sc1 == sc2.key) return <div className="absolute z-50 left-[100%] top-0 min-h-[100%] w-[200px] hidden group-hover/sub:block border-l-2 border-[#bfbfbf] py-2" key={i}>
-                                        {sc2.category.map((c2, i) => <div key={i}>
-                                            {c2.category}
-                                        </div>)}
-                                    </div>
-                                })}
-                            </div>)}
+                        if (sc.key == c.key) return <div key={i} className="absolute z-50 left-[calc(100%+10px)] top-0 min-h-[100%] w-[200px] hidden group-hover:flex flex-col bg-white">
+                            <div className="relative flex-grow">
+                                {sc.category.map((sc1, i) => <div className='group/sub' key={i}>
+                                    <span>{sc1}</span>
+                                    {subCategories2.map((sc2, i) => {
+                                        if (sc1 == sc2.key) return <div className="bg-[rgba(0,0,0,0.1)] absolute z-50 left-[100%] top-0 min-h-[100%] w-[200px] hidden group-hover/sub:flex flex-col bg-white" key={i}>
+                                            <div className="my-[10px] py-[10px] border-l-2 border-[#bfbfbf] flex-grow flex flex-row">
+                                                {sc2.category.map((c2, i) => <Link href={c2.category} key={i}>
+                                                    {c2.category}
+                                                </Link>)}
+                                            </div>
+                                        </div>
+                                    })}
+                                </div>)}
+                            </div>
                         </div>
                     })}
                 </div>)}
             </div>
-            {/* <div className="w-[80%]">
+            <div className="w-[80%]">
                 <SliderPage />
-            </div> */}
+            </div>
         </div>
     )
 }
--- app/_components/hero/slider.tsx
+++ app/_components/hero/slider.tsx
@@ -1,10 +1,43 @@
 'use client'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Image from 'next/image';
+
+interface ArrowProps {
+    className: string,
+    style: React.CSSProperties,
+    onClick: () => void,
+    arrow: boolean,
+}
+
+function NextArrow(props: ArrowProps) {
+    const { className, style, onClick, arrow } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, opacity: arrow ? 1 : 0, background: 'rgba(0,0,0,0.3)', width: '21px', height: '40px' }}
+            onClick={onClick}
+        />
+    );
+}
+
+function PrevArrow(props: ArrowProps) {
+    const { className, style, onClick, arrow } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, opacity: arrow ? 1 : 0, background: 'rgba(0,0,0,0.2)', width: '21px', height: '40px' }}
+            onClick={onClick}
+        />
+    );
+}
 
 export default function SliderPage() {
+    const images = ['/slider1.jpg', '/slider2.jpg'];
+    const slider = useRef<Slider>(null);
+    const [showArrow, setShowArrow] = useState<boolean>(true);
     var settings = {
         dots: true,
         infinite: true,
@@ -12,27 +45,19 @@ export default function SliderPage() {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
+        prevArrow: <PrevArrow className='' style={{}} arrow={showArrow} onClick={() => slider.current?.slickPrev()} />,
+        nextArrow: <NextArrow className='' style={{}} arrow={showArrow} onClick={() => slider.current?.slickNext()} />,
+        beforeChange: () => {
+            console.log('aalu')
+            setShowArrow(false)
+        },
+        afterChange: () => setShowArrow(true),
     };
     return (
-        <Slider {...settings}>
-            <div>
-                <h3>1</h3>
-            </div>
-            <div>
-                <h3>2</h3>
-            </div>
-            <div>
-                <h3>3</h3>
-            </div>
-            <div>
-                <h3>4</h3>
-            </div>
-            <div>
-                <h3>5</h3>
-            </div>
-            <div>
-                <h3>6</h3>
-            </div>
+        <Slider {...settings} ref={slider}>
+            {images.map((img, i) => {
+                return <Image key={i} alt='Slider Image' src={img} width={1000} height={400} className='p-[1px]' />
+            })}
         </Slider>
     )
 }
