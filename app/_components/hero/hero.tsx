5f1829502b67bba41dba7308e45a7847c72d14e7
bidursapkota00 <bidursapkota00@gmail.com>
2023-12-14 21:06:19 +0545

    space between absolute box and parent, achieved with transparent background

--- app/_components/hero/hero.tsx
+++ app/_components/hero/hero.tsx
@@ -37,20 +37,17 @@ export default function Hero() {
     return (
         <div className='flex flex-row py-[10px] px-[76px] bg-[#f5f5f5]'>
             <div className="w-[20%] relative border border-[#bfbfbf] bg-white mr-[50px]">
-                {categories.map((c, i) => <div key={i} className="grid grid-cols-[30px_1fr_0px] cursor-pointer group w-[100%]">
+                {categories.map((c, i) => <div key={i} className="grid grid-cols-[30px_1fr] cursor-pointer group w-[100%]">
                     {c.icon}
                     <span>{c.text}</span>
-                    <div className='w-0 justify-self-end relative'>
-                        <div className="absolute z-50 left-[100%] top-0 h-[100%] w-[15px]"></div>
-                    </div>
                     {subCategories.map((sc, i) => {
-                        if (sc.key == c.key) return <div key={i} className="absolute z-50 left-[calc(100%+10px)] top-0 min-h-[100%] w-[200px] hidden group-hover:flex flex-col bg-white">
-                            <div className="relative flex-grow">
+                        if (sc.key == c.key) return <div key={i} className="absolute z-50 left-[100%] top-0 min-h-[100%] w-[200px] hidden group-hover:flex flex-col bg-transparent">
+                            <div className="relative flex-grow ml-3 bg-white">
                                 {sc.category.map((sc1, i) => <div className='group/sub' key={i}>
                                     <span>{sc1}</span>
                                     {subCategories2.map((sc2, i) => {
-                                        if (sc1 == sc2.key) return <div className="bg-[rgba(0,0,0,0.1)] absolute z-50 left-[100%] top-0 min-h-[100%] w-[200px] hidden group-hover/sub:flex flex-col bg-white" key={i}>
-                                            <div className="my-[10px] py-[10px] border-l-2 border-[#bfbfbf] flex-grow flex flex-row">
+                                        if (sc1 == sc2.key) return <div className="absolute z-50 left-[100%] top-0 min-h-[100%] w-[200px] hidden group-hover/sub:flex flex-col bg-white" key={i}>
+                                            <div className="my-[10px] px-[10px] border-l-2 border-[#bfbfbf] flex-grow flex flex-row flex-wrap justify-between">
                                                 {sc2.category.map((c2, i) => <Link href={c2.category} key={i}>
                                                     {c2.category}
                                                 </Link>)}
