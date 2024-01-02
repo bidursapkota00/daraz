24d64fc206d86ba5c29df23c56a362f318f01399
bidursapkota00 <bidursapkota00@gmail.com>
2024-01-02 17:21:12 +0545

    minor changes

--- app/_components/footer/footer.tsx
+++ app/_components/footer/footer.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className='text-[#888] columns-4 gap-9 bg-[#f1f1f1] px-[0px] text-[12px] py-6'>
+        <div className='text-[#888] columns-4 gap-9 bg-[#f1f1f1] text-[12px] py-6 px-[76px]'>
             <h1 className='text-[#606060] break-inside-avoid-column text-[18px] leading-5 mb-[18px]'>Experience Hassle-Free Online Shopping in Nepal with Daraz</h1>
             <p className='break-inside-avoid-column'>E-commerce has evolved over the past few years and since it&apos;s easier and more convenient, it is evident that customers are actually switching to the trend of online shopping. Daraz, the Nepali shopping store, brings a whole new concept by showcasing a number of famous brands under one roof. Not only does it fulfill clothing necessities of both men and women but you can also shop for all kinds of appliances like air conditioners, heaters, refrigerators, LED TVs and a lot more. Simply select your favorite brand like Samsung, Apple, HP, Huawei, Dell, Canon, Nikon, etc and get yourself the best electronic items.</p>
             <h2 className='text-[#606060] break-inside-avoid-column text-[16px] my-[18px]'>Convenient Online Shopping in Nepal</h2>
             <p className='break-inside-avoid-column'>Daraz is the ultimate Nepali eCommerce website that offers a solution for all needs of the customers. It has a wide and assorted range of products including clothing, electronics, mobile phones, home and living, health and beauty and much more.</p>
-            <p className='break-inside-avoid-column mt-[18px]'>Daraz strives to provide customers the best shopping experience in Nepal. The online store is updated daily and new products are added every day to cater to all your needs. Visit Daraz.com.np to experience shopping in Nepal as never before. Don't forget to Download Daraz App and get exclusive discounts.</p>
-            <p className='break-inside-avoid-column'>is updated daily and new products are added every day to cater to all your needs. Visit Daraz.com.np to experience shopping in Nepal as never before. Don't forget to Download Daraz App and get exclusive discounts.</p>
+            <p className='break-inside-avoid-column mt-[18px]'>Daraz strives to provide customers the best shopping experience in Nepal. The online store is updated daily and new products are added every day to cater to all your needs. Visit Daraz.com.np to experience shopping in Nepal as never before. Don&apos;t forget to Download Daraz App and get exclusive discounts.</p>
+            <p className='break-inside-avoid-column'>is updated daily and new products are added every day to cater to all your needs. Visit Daraz.com.np to experience shopping in Nepal as never before. Don&apos;t forget to Download Daraz App and get exclusive discounts.</p>
             <p className="break-inside-avoid-column mt-[18px]">Daraz is a global online marketplace with ecommerce stores in <Link className='text-[#199cb7] hover:underline' href='#'>Pakistan</Link>, <Link className='text-[#199cb7] hover:underline' href='#'>Bangladesh</Link>, <Link className='text-[#199cb7] hover:underline' href='#'>Nepal</Link>, <Link className='text-[#199cb7] hover:underline' href='#'>Sri Lanka</Link> and <Link className='text-[#199cb7] hover:underline' href='#'>Myanmar</Link>.</p>
             <p className="break-inside-avoid-column my-[18px]">General Information:</p>
             <p className="break-inside-avoid-column">Daraz Kaymu Private Limited Kathmandu Metropolitan City, Ward No. 11,Thapathali,Kathmandu, Nepal VAT No. 602403687</p>
@@ -21,7 +21,7 @@ export default function Footer() {
                 <Link className='hover:underline' href='#'>12.12 Sale</Link>&nbsp;,&nbsp;
                 <Link className='hover:underline' href='#'>Dashain Dhamaka</Link>&nbsp;,&nbsp;
                 <Link className='hover:underline' href='#'>Mobile Week</Link>&nbsp;,&nbsp;
-                <Link className='hover:underline' href='#'>Valentine's Day Sale</Link>&nbsp;,&nbsp;
+                <Link className='hover:underline' href='#'>Valentine&apos;s Day Sale</Link>&nbsp;,&nbsp;
                 <Link className='hover:underline' href='#'>Cricket Streaming</Link>&nbsp;,&nbsp;
                 <Link className='hover:underline' href='#'>Grocery Shopping in Nepal</Link>&nbsp;,&nbsp;
                 <Link className='hover:underline' href='#'>Realme GT 2 Pro</Link>&nbsp;,&nbsp;
@@ -95,7 +95,7 @@ export default function Footer() {
                 <Link className='hover:underline' href='#'>Nokia Mobiles</Link>&nbsp;,&nbsp;
                 <Link className='hover:underline' href='#'>Nokia Mobiles</Link>
             </p>
-            <div className='mt-[12px]'><Link className='text-[#333] hover:underline' href="#">WOMEN'S FASHION</Link></div>
+            <div className='mt-[12px]'><Link className='text-[#333] hover:underline' href="#">WOMEN&apos;S FASHION</Link></div>
             <p className="break-inside-avoid-column">
                 <Link className='hover:underline' href='#'>Samsung Mobile Phones</Link>&nbsp;,&nbsp;
                 <Link className='hover:underline' href='#'>Xiaomi Mobiles</Link>&nbsp;,&nbsp;
@@ -105,7 +105,7 @@ export default function Footer() {
                 <Link className='hover:underline' href='#'>Nokia Mobiles</Link>&nbsp;,&nbsp;
                 <Link className='hover:underline' href='#'>Nokia Mobiles</Link>
             </p>
-            <div className='mt-[12px]'><Link className='text-[#333] hover:underline' href="#">MEN'S FASHION</Link></div>
+            <div className='mt-[12px]'><Link className='text-[#333] hover:underline' href="#">MEN&apos;S FASHION</Link></div>
             <p className="break-inside-avoid-column">
                 <Link className='hover:underline' href='#'>Samsung Mobile Phones</Link>&nbsp;,&nbsp;
                 <Link className='hover:underline' href='#'>Xiaomi Mobiles</Link>&nbsp;,&nbsp;
